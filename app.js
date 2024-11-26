9aaf400f3f0c13194b4d04c8c97d5d88d283a855
Ahmed Raouf <ahmedraouf.metwally@gmail.com>
2024-11-26 07:39:31 +0200

    update swagger.yaml

--- app.js
+++ app.js
@@ -33,13 +33,10 @@ app.use( express.json() );
 
 // extra packages
 app.set( 'trust proxy', 1 );
-// app.use( rateLimit( {
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-//   standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-//   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-//   // store: ... , // Redis, Memcached, etc. See below.
-// } ) );
+app.use( rateLimit( {
+  windowMs: 60 * 1000,
+  max: 60,
+} ) );
 app.use( cors() );
 app.use( xss() );
 app.use( helmet() );
